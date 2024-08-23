764f96a760a379c7d476d60dbafbd22dadf7a28d
Leonardo Melgarejo <leonardo_melgarejo@hotmail.com>
2024-08-23 17:04:39 +0100

    Adicionando descrições

--- src/test/java/br/ce/wcaquino/rest/tests/BarrigaTest.java
+++ src/test/java/br/ce/wcaquino/rest/tests/BarrigaTest.java
@@ -1,14 +1,17 @@
 package br.ce.wcaquino.rest.tests;
 
 import br.ce.wcaquino.rest.core.BaseTest;
+import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 
 import static io.restassured.RestAssured.given;
 
+@DisplayName("BarrigaTest")
 public class BarrigaTest extends BaseTest {
 
     @Test
-    public void naoDeveAcessarAPISemToke(){
+    @DisplayName("Não deve acessar o recurso /contas sem token")
+    public void naoDeveAcessarAPISemToken(){
         given()
         .when()
             .get("/contas")
