c538afcf3e17399fb0d13371e68df53b2daf5ffc
Leonardo Melgarejo <leonardo_melgarejo@hotmail.com>
2024-08-26 10:47:17 +0100

    deveIncluirContaComSucesso

--- src/test/java/br/ce/wcaquino/rest/tests/BarrigaTest.java
+++ src/test/java/br/ce/wcaquino/rest/tests/BarrigaTest.java
@@ -1,8 +1,12 @@
 package br.ce.wcaquino.rest.tests;
 
 import br.ce.wcaquino.rest.core.BaseTest;
+import io.restassured.response.Response;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
+
+import java.util.HashMap;
+import java.util.Map;
 
 import static io.restassured.RestAssured.given;
 
@@ -17,6 +21,31 @@ public class BarrigaTest extends BaseTest {
             .get("/contas")
         .then()
             .statusCode(401)
+        ;
+    }
+
+    @Test
+    @DisplayName("Deve incluir conta com suesso")
+    public void deveIncluirContaComSucesso(){
+        Map<String, String> login = new HashMap<>();
+        login.put("email","melgarejom.leonardo@gmail.com");
+        login.put("senha", "1234");
+
+        String token = given()
+                    .body(login)
+                .when()
+                    .post("/signin")
+                .then()
+                    .statusCode(200)
+                    .extract().path("token");
+
+        given()
+            .header("Authorization", "JWT " + token)
+            .body("{ \"nome\": \"conta qualquer\" }")
+        .when()
+            .post("/contas")
+        .then()
+            .statusCode(201)
         ;
     }
 }
