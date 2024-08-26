2ff00f46a41597840582dce77505441b9cb35377
Leonardo Melgarejo <leonardo_melgarejo@hotmail.com>
2024-08-26 11:55:37 +0100

    deveAlterarContaComSucesso

--- src/test/java/br/ce/wcaquino/rest/tests/BarrigaTest.java
+++ src/test/java/br/ce/wcaquino/rest/tests/BarrigaTest.java
@@ -1,7 +1,7 @@
 package br.ce.wcaquino.rest.tests;
 
 import br.ce.wcaquino.rest.core.BaseTest;
-import io.restassured.response.Response;
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 
@@ -9,9 +9,27 @@ import java.util.HashMap;
 import java.util.Map;
 
 import static io.restassured.RestAssured.given;
+import static org.hamcrest.Matchers.*;
 
 @DisplayName("BarrigaTest")
 public class BarrigaTest extends BaseTest {
+
+    private String TOKEN;
+
+    @BeforeEach
+    public void login(){
+        Map<String, String> login = new HashMap<>();
+        login.put("email","melgarejom.leonardo@gmail.com");
+        login.put("senha", "1234");
+
+        TOKEN = given()
+            .body(login)
+        .when()
+            .post("/signin")
+        .then()
+            .statusCode(200)
+            .extract().path("token");
+    }
 
     @Test
     @DisplayName("Não deve acessar o recurso /contas sem token")
@@ -25,27 +43,29 @@ public class BarrigaTest extends BaseTest {
     }
 
     @Test
-    @DisplayName("Deve incluir conta com suesso")
+    @DisplayName("Deve incluir conta com sucesso")
     public void deveIncluirContaComSucesso(){
-        Map<String, String> login = new HashMap<>();
-        login.put("email","melgarejom.leonardo@gmail.com");
-        login.put("senha", "1234");
-
-        String token = given()
-                    .body(login)
-                .when()
-                    .post("/signin")
-                .then()
-                    .statusCode(200)
-                    .extract().path("token");
-
         given()
-            .header("Authorization", "JWT " + token)
+            .header("Authorization", "JWT " + TOKEN)
             .body("{ \"nome\": \"conta qualquer\" }")
         .when()
             .post("/contas")
         .then()
             .statusCode(201)
+        ;
+    }
+
+    @Test
+    @DisplayName("Deve alterar conta com sucesso")
+    public void deveAlterarContaComSucesso(){
+        given()
+            .header("Authorization", "JWT " + TOKEN)
+            .body("{ \"nome\": \"conta alterada\" }")
+        .when()
+            .put("/contas/2239295")
+        .then()
+            .statusCode(200)
+            .body("nome", is("conta alterada"))
         ;
     }
 }
