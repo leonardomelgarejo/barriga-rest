be4cdd4a97ae6eed15df73fe221160696ed3b210
Leonardo Melgarejo <leonardo_melgarejo@hotmail.com>
2024-08-26 12:12:52 +0100

    deveInserirMovimentacaoComSucesso

--- src/test/java/br/ce/wcaquino/rest/tests/BarrigaTest.java
+++ src/test/java/br/ce/wcaquino/rest/tests/BarrigaTest.java
@@ -83,4 +83,28 @@ public class BarrigaTest extends BaseTest {
         ;
     }
 
+    @Test
+    @DisplayName("Deve inserir movimentacao com sucesso")
+    public void deveInserirMovimentacaoComSucesso(){
+        Movimentacao mov = new Movimentacao();
+        mov.setConta_id(2239295);
+//        mov.setUsuario_id(usuario_id);
+        mov.setDescricao("Descrição da movimentação");
+        mov.setEnvolvido("Envolvido na mov");
+        mov.setTipo("REC");
+        mov.setData_transacao("01/01/2000");
+        mov.setData_pagamento("10/05/2010");
+        mov.setValor(100f);
+        mov.setStatus(true);
+
+        given()
+            .header("Authorization", "JWT " + TOKEN)
+            .body(mov)
+        .when()
+            .post("/transacoes")
+        .then()
+            .statusCode(201)
+        ;
+    }
+
 }
